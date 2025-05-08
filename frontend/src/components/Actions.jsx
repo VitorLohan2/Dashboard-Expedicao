21aa7651cf27a8040f546c06fff68d756aef3b62
VitorLohan2 <victor.l@grupodime.com.br>
2025-05-08 17:33:10 -0300

    ATUALIZAÇÃO 1.1 - 08/05/25
    AJUSTES

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,8 +1,7 @@
 // src/components/Actions.jsx
 import React from 'react';
 
-
-const Actions = ({ onStart, onFinish }) => {
+const Actions = ({ onStart, onFinish, onPause, isPaused }) => {
   return (
     <div className="actions">
       <button className="btn iniciar" onClick={onStart}>
@@ -11,9 +10,13 @@ const Actions = ({ onStart, onFinish }) => {
       <button className="btn finalizar" onClick={onFinish}>
         ✅ Finalizar Carregamento
       </button>
+      <button className="btn pausar" onClick={onPause}>
+        {isPaused ? '▶️ Retomar Carregamento' : '⏸️ Pausar Carregamento'}
+      </button>
     </div>
   );
 };
 
 export default Actions;
+
 
