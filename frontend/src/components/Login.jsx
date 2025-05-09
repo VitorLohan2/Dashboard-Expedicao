7f148ea4252c3dd4fd4c4f55f438baaf831d839d
VitorLohan2 <victor.l@grupodime.com.br>
2025-05-09 19:16:17 -0300

    Att 09/05

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';   // No seu handleLogin, adicione navegação após login bem-sucedido
 import logo from '../assets/logo2.png';
-import api from '../services/api';
+import api from '../services/api'; /* SERVICE */
 
 function Login({ onLogin }) {
   const [usuario, setUsuario] = useState('');
